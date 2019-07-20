64c1f5583fbbee1b881aaed128cd3bec3857c5ce
Pranav Kadam <pdkadam@buffalo.edu>
2019-07-20 12:58:02 -0400

    Used a stack instead of a linked list
    Found out while debugging that a stack will point me to the right solution.
    This solution works for only 2 cases (1) and (1,2).
    Will review and make changes to the solution.

--- linkedlistsol.java
+++ linkedlistsol.java
@@ -4,37 +4,43 @@ import java.util.LinkedList;
 
 public class towerOfHanoiSolution{
     
-    public static LinkedList<Integer> removeThis(LinkedList<Integer> q1){
+    public static Stack<Integer> removeLast(Stack<Integer> q1){
         
-        if(q1.size() == 1){
-            return q1;
+        int size = q1.size()-1;
+        
+        Stack<Integer> q2 = new Stack<Integer>();
+        
+        int i = 0;
+        
+        while(i!=size){
+            q2.push(q1.pop());
+            i++;
         }
-        
-        q1.remove(q1.size()-1);
-        return q1;
+        return (q2.isEmpty())? q1 : q2;
     }
     
-    public static void towerOfHanoi(int n, LinkedList<Integer> source, LinkedList<Integer> target, LinkedList<Integer> spare){
+    public static void towerOfHanoi(int n, Stack<Integer> source, Stack<Integer> target, Stack<Integer> spare){
 
         if (n==1){
-            target.add(source.remove());
+            target.push(source.pop());
             return;
         }
         
-        towerOfHanoi(n-1, removeThis(source), spare, target);
-        target.add(source.remove());
+        towerOfHanoi(n-1, removeLast(source), spare, target);
+        target.push(source.pop());
         System.out.println(target);
-        towerOfHanoi(n-1, removeThis(source), target, source);
+        towerOfHanoi(n-1, removeLast(spare), target, source);
         
     }
 
      public static void main(String []args){
-        LinkedList<Integer> q1 = new LinkedList<>();
-        LinkedList<Integer> q2 = new LinkedList<>();
-        LinkedList<Integer> q3 = new LinkedList<>();
+        Stack<Integer> q1 = new Stack<Integer>();
+        Stack<Integer> q2 = new Stack<Integer>();
+        Stack<Integer> q3 = new Stack<Integer>();
         
         q1.add(1);
         q1.add(2);
+        
         System.out.println(q1);
         
         
